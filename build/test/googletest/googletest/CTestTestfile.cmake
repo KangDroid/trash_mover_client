d53a2a58cd23d07bfe97074ddbdb5bd4d22c3794
KangDroid <hyunwoo286@gmail.com>
2021-01-18 17:18:13 +0900

    Remove build folder

--- build/test/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kangdroid/CLionProjects/TrashMoverClient/test/googletest/googletest
-# Build directory: /Users/kangdroid/CLionProjects/TrashMoverClient/build/test/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
